2a2d8ad13a1eaadcb1169d25743af4180f000700
Hamsi2007 <102812720+Hamsi2007@users.noreply.github.com>
2022-04-06 18:16:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+var Monkey=
+[
+"https://s3-projectsassets.whjr.online/Monkey_02.png",
+"https://s3-projectsassets.whjr.online/Monkey_03.png",
+"https://s3-projectsassets.whjr.online/Monkey_04.png",
+"https://s3-projectsassets.whjr.online/Monkey_05.png",
+"https://s3-projectsassets.whjr.online/Monkey_06.png",
+"https://s3-projectsassets.whjr.online/Monkey_07.png",
+"https://s3-projectsassets.whjr.online/Monkey_08.png",
+"https://s3-projectsassets.whjr.online/Monkey_09.png",
+"https://s3-projectsassets.whjr.online/Monkey_10.png",
+"https://s3-projectsassets.whjr.online/Monkey_01.png"
+
+];
+i=0;
+function next()
+{
+if(i>=10)
+  {i=0;}
+  document.getElementById("Monkey").src=Monkey[i];
+  i++;
+}
